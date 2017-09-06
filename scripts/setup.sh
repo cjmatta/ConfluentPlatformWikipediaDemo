0670f55d13396c7833059406953b1a25000d2641
Yeva Byzek <yeva@confluent.io>
2017-09-06 15:24:06 -0400

    Give feedback in startup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Optionally rename the cluster with the following commands from your host machine:
+echo -e "\nRename the cluster with the following commands from your host machine:"
 curl -X PATCH  -H "Content-Type: application/merge-patch+json" -d '{"displayName":"Kafka East"}' http://localhost:9021/2.0/clusters/kafka/$(curl -X get http://localhost:9021/2.0/clusters/kafka/ | jq --raw-output .[0].clusterId)
 # If you don't have `jq`
 #curl -X PATCH  -H "Content-Type: application/merge-patch+json" -d '{"displayName":"Kafka East"}' http://localhost:9021/2.0/clusters/kafka/$(curl -X get http://localhost:9021/2.0/clusters/kafka/ | awk -v FS="(clusterId\":\"|\",\"displayName)" '{print $2}' )
 
-# Start streaming from IRC, source connector:
+echo -e "\nStart streaming from IRC, source connector:"
 ./scripts/submit_wikipedia_irc_config.sh
 
-# Tell Elasticsearch what the data looks like:
+echo -e "\nTell Elasticsearch what the data looks like:"
 ./scripts/set_elasticsearch_mapping.sh
 
-# Start sending data to Elasticsearch, sink connector:
+echo -e "\nStart sending data to Elasticsearch, sink connector:"
 ./scripts/submit_elastic_sink_config.sh
 
-# Configure Kibana settings:
+echo -e "\nConfigure Kibana settings:"
 ./scripts/configure_kibana_dashboard.sh
