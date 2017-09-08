b8ba1d8b3447e30fe22b7a2243fedf6771692944
Chris Matta <chris@confluent.io>
2017-09-08 17:16:19 -0400

    Adding the null filter jar and interceptors jar for now to make the demo work

--- scripts/listen_wikipedia.parsed.sh
+++ scripts/listen_wikipedia.parsed.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-docker exec confluentplatformwikipediademo_connect_1 kafka-avro-console-consumer \
-  --property schema.registry.url=http://schemaregistry:8081 \
+docker exec confluentplatformwikipediademo_connect_1 kafka-console-consumer \
   --bootstrap-server kafka:9092 --topic wikipedia.parsed --new-consumer
