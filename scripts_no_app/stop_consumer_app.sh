903aee6e264bba638d0192c87516030fab14d1a7
Yeva Byzek <yeva@confluent.io>
2017-09-11 07:27:37 -0400

    Updated C3 image to 3.3.1-alpha2; enhanced stop_consumer_app script to kill on container as well

--- scripts_no_app/stop_consumer_app.sh
+++ scripts_no_app/stop_consumer_app.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 kill $(ps aux | grep confluentplatformwikipediademo_connect_1 | grep -v "grep confluentplatformwikipediademo_connect_1" | awk '{print $2}')
+docker exec confluentplatformwikipediademo_connect_1 kill $(ps aux | grep consumer | grep -v "grep consumer" | awk '{print $2}')
 
